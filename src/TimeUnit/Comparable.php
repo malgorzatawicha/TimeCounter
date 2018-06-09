068dee956032b953db537200d892cea10f0aecff
Małgorzata Wicha <malgorzata.wicha@gmail.com>
2018-06-09 12:44:18 +0200

    Add methods to Comparable that implements Allen's time interval algebra.

--- src/TimeUnit/Comparable.php
+++ src/TimeUnit/Comparable.php
@@ -3,5 +3,17 @@ namespace TimeCounter\TimeUnit;
 
 interface Comparable
 {
-
+    public function precedes(Comparable $comparable): bool;
+    public function meets(Comparable $comparable): bool;
+    public function overlaps(Comparable $comparable): bool;
+    public function finishedBy(Comparable $comparable): bool;
+    public function contains(Comparable $comparable): bool;
+    public function starts(Comparable $comparable): bool;
+    public function equals(Comparable $comparable): bool;
+    public function startedBy(Comparable $comparable): bool;
+    public function during(Comparable $comparable): bool;
+    public function finishes(Comparable $comparable): bool;
+    public function overlappedBy(Comparable $comparable): bool;
+    public function metBy(Comparable $comparable): bool;
+    public function precededBy(Comparable $comparable): bool;
 }
