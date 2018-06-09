51877df099fd17938088418cdc999642360a71a8
Małgorzata Wicha <malgorzata.wicha@gmail.com>
2018-06-09 12:45:55 +0200

    Implement time interval relation precedes in YearTimeUnit

--- src/TimeUnit/YearTimeUnit.php
+++ src/TimeUnit/YearTimeUnit.php
@@ -4,4 +4,89 @@ namespace TimeCounter\TimeUnit;
 class YearTimeUnit implements Comparable
 {
 
+    /**
+     * @var string
+     */
+    private $year;
+
+    public function __construct(string $year)
+    {
+        $this->year = $year;
+    }
+
+    protected function getYear()
+    {
+        return $this->year;
+    }
+
+    public function precedes(Comparable $comparable): bool
+    {
+        if ($comparable instanceof YearTimeUnit) {
+            return $this->getYear() < $comparable->getYear();
+        }
+        return false;
+    }
+
+    public function meets(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function overlaps(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function finishedBy(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function contains(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function starts(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function equals(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function startedBy(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function during(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function finishes(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function overlappedBy(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function metBy(Comparable $comparable): bool
+    {
+        return false;
+    }
+
+    public function precededBy(Comparable $comparable): bool
+    {
+        if ($comparable instanceof YearTimeUnit) {
+            return $comparable->getYear() < $this->getYear();
+        }
+        return false;
+    }
 }
--- tests/TimeUnit/YearTimeUnitTest.php
+++ tests/TimeUnit/YearTimeUnitTest.php
@@ -12,4 +12,31 @@ class YearTimeUnitTest extends TestCase
         $year = new YearTimeUnit('2017');
         $this->assertInstanceOf(Comparable::class, $year);
     }
+
+    /**
+     * @dataProvider yearsPrecedesOtherYears
+     * @param string $firstYearValue
+     * @param string $secondYearValue
+     * @param bool $expectedResult
+     */
+    public function testCheckingOneYearPrecedesSecondReturnsCorrectResults(
+        string $firstYearValue,
+        string $secondYearValue,
+        bool $expectedResult
+    ) {
+        $firstYear = new YearTimeUnit($firstYearValue);
+        $secondYear = new YearTimeUnit($secondYearValue);
+
+        $this->assertEquals($expectedResult, $firstYear->precedes($secondYear));
+    }
+
+    public function yearsPrecedesOtherYears()
+    {
+        return [
+            '2015 precedes 2017' => ['2015', '2018', true],
+            '2016 precedes 2017' => ['2016', '2017', true],
+            '2017 does not precede 2017' => ['2017', '2017', false],
+            '2018 does not precede 2017' => ['2018', '2017', false],
+        ];
+    }
 }
